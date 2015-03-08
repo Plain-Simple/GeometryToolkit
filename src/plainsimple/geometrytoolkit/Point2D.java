58bc7bc71285a36e7ec6e866bf5755c6b7ba55fb
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-08 12:41:23 -0400

    Same Methods as Point3D

---
+++ src/plainsimple/geometrytoolkit/Point2D.java
@@ -0,0 +1,28 @@
+package plainsimple.geometrytoolkit;
+
+public class Point2D {
+    private double x;
+    private double y;
+    private String name;
+    public Point2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    /* returns x */
+    public double getX() {
+        return x;
+    }
+    /* returns y */
+    public double getY() {
+        return y;
+    }
+    /* returns String representation of point */
+    public String getPoint() {
+        return "(" + x + ", " + y + ")";
+    }
+    /* returns distance between points */
+    public double getDistance(Point3D point_2) {
+        return Math.sqrt(Math.pow((x - point_2.getX()), 2) +
+                Math.pow((y - point_2.getY()), 2));
+    }
+}
