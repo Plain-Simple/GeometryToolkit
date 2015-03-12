914b4a5996d7372da165b012d483afff120c82a4
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-12 16:42:08 -0400

    More Strings

--- src/plainsimple/geometrytoolkit/Messages.java
+++ src/plainsimple/geometrytoolkit/Messages.java
@@ -16,6 +16,57 @@ public interface Messages {
   @En("created")
   String created();
 
-  @En("Error ocurred")
+  @En("Error occurred")
   String generic_error();
+
+    @En("Error: ")
+    String error();
+
+    @En("Variable ")
+    String variable();
+
+    @En(" does not exist")
+    String var_does_not_exist();
+
+    @En(" or constructor is invalid")
+    String invalid_constructor();
+
+    @En("Type \"help\" for usage information")
+    String type_help();
+
+    @En("\"")
+    String double_quote();
+
+    @En(" ")
+    String space();
+
+    @En("|")
+    String magnitude_symbol();
+
+    @En(" = ")
+    String equal_sign();
+
+    @En(" is not parallel to ")
+    String not_parallel();
+
+    @En(" is parallel to ")
+    String parallel();
+
+    @En(" is not perpendicular to ")
+    String not_perpendicular();
+
+    @En(" is perpendicular to ")
+    String perpendicular();
+
+    @En(" is equal to ")
+    String equal_to();
+
+    @En(" is not equal to ")
+    String not_equal_to();
+
+    @En("Argument")
+    String argument();
+
+    @En(" was not recognized")
+    String not_recognized();
 }
