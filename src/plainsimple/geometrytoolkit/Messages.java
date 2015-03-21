960c3b62f36a2b0c80d38bdcda6278d5d211c168
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-20 22:05:31 -0400

    Added help function(s)

--- src/plainsimple/geometrytoolkit/Messages.java
+++ src/plainsimple/geometrytoolkit/Messages.java
@@ -64,7 +64,7 @@ public interface Messages {
     @En(" is not equal to ")
     String not_equal_to();
 
-    @En("Argument")
+    @En("Argument ")
     String argument();
 
     @En(" was not recognized")
@@ -102,5 +102,33 @@ public interface Messages {
 
     @En("Error creating object ")
     String error_creating_object();
+
+    @En("The GeometryToolkit allows users to create objects, store them as variables, and manipulate them.\n" +
+            "Currently supported objects:\n" +
+            "Vector3D - A three dimensional vector in component form <x,y,z>\n" +
+            "Point3D  - A three dimensional point in the form (x,y,z)\n" +
+            "Plane3D  - A three dimensional plane represented by a Cartesian Equation\n" +
+            "Line3D   - A three dimensional line represented as (x,y,z) + t<x,y,z>\n" +
+            "Variables can be constructed using <VariableName> = <Variable> <Operator> <Variable>\n" +
+            "or by using the Object's specialized constructor. \n" +
+            "The command \"construct <ObjectType>\" can guide you through the process of creating that specific object.\n" +
+            "For more detailed information on specific objects and their constructors, use \"help <ObjectType>\"")
+    String general_help();
+
+    @En("THE VECTOR3D OBJECT\n" +
+            "Constructors:\n" +
+            "\"<name> = <x,y,z>\" creates a Vector3D variable named <name> with component form <x,y,z>\n\n" +
+            "Supported Operators:\n" +
+            " <Vector3D>                  Outputs Vector in component form\n" +
+            "|<Vector3D|                  Magnitude calculation\n" +
+            "<Vector3D> +/- <Vector3D>    Vector addition/subtraction\n" +
+            "<Vector3D>  *  <Vector3D>    Vector dot product\n" +
+            "<Vector3D>  *  < Number >    Scalar multiplication\n" +
+            "<Vector3D>  /  < Number >    Scalar division\n"+
+            "<Vector3D>  x  <Vector3D>    Vector cross product\n" +
+            "<Vector3D>  // <Vector3D>    Returns TRUE if vectors are parallel, otherwise FALSE\n" +
+            "<Vector3D>  |_ <Vector3D>    Returns TRUE if vectors are perpendicular, otherwise FALSE\n" +
+            "<Vector3D>  == <Vector3D>    Returns TRUE if vectors are equivalent, otherwise FALSE")
+    String vector3d_help();
 
 }
