698c082179345542914141cf252712c1b495685f
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-28 15:17:03 -0400

    Methods for Removing Object(s) and Saving Them to File

--- src/plainsimple/geometrytoolkit/StoredObjects.java
+++ src/plainsimple/geometrytoolkit/StoredObjects.java
@@ -1,10 +1,11 @@
 package plainsimple.geometrytoolkit;
 
+import java.io.*;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import c10n.C10N;
 
-public class StoredObjects {
+public class StoredObjects { // todo: constructor that takes filename?
     /* stores user objects */
     private  Hashtable<String, Object> user_objects = new Hashtable<>();
     /* gets object from hashtable. If object does not exist, tries to construct it */
@@ -26,30 +27,64 @@ public class StoredObjects {
     }
     /* returns String listing all objects (for list() command) */
     public String list() {
-        String result = "";
-        for(Enumeration<String> objects = user_objects.keys(); objects.hasMoreElements();)
-            result += objects.nextElement() + " -> " + user_objects.get(objects.nextElement());
+        String result = "Stored Objects\n";
+        Enumeration<String> objects = user_objects.keys();
+        while(objects.hasMoreElements()) {
+            String object_name = objects.nextElement();
+            result += object_name + " -> " +
+                    (new GeometryObject(user_objects.get(object_name))).toString() + "\n";
+        }
         return result;
     }
     /* returns String listing all objects of object_type */
     public String list(String object_type) {
-        try{ // todo: testing!
-            Class specified_class = Class.forName(object_type);
-            String result = "";
-            for(Enumeration<String> objects = user_objects.keys(); objects.hasMoreElements();) {
-                if(user_objects.get(objects.nextElement()).getClass().equals(specified_class))
-                    result += objects.nextElement() + " -> " +
-                            (new GeometryObject(user_objects.get(objects.nextElement()))).toString();
-            }
-            return result;
-        }catch(ClassNotFoundException e) {
-            return msg.class_not_recognized(object_type);
+        String result = "";
+        Class list_class;
+        switch (object_type) {
+            case "Vector3D":
+                list_class = Vector3D.class;
+                result += "Stored Vector3D Objects\n";
+                break;
+            case "Vector2D":
+                list_class = Vector2D.class;
+                result += "Stored Vector2D Objects\n";
+                break;
+            case "Point3D":
+                list_class = Point3D.class;
+                result += "Stored Point3D Objects\n";
+                break;
+            case "Point2D":
+                list_class = Point2D.class;
+                result += "Stored Point2D Objects\n";
+                break;
+            case "Plane3D":
+                list_class = Plane3D.class;
+                result += "Stored Plane3D Objects\n";
+                break;
+            case "Line3D":
+                list_class = Line3D.class;
+                result += "Stored Line3D Objects\n";
+                break;
+            default:
+                list_class = Double.class; // todo: finish this. Default should give an error - unrecognized class
         }
+        Enumeration<String> objects = user_objects.keys();
+        while(objects.hasMoreElements()) {
+            String object_name = objects.nextElement();
+            if(user_objects.get(object_name).getClass().equals(list_class))
+                result += object_name + " -> " +
+                    (new GeometryObject(user_objects.get(object_name))).toString() + "\n";
+        }
+        return result;
+    }
+    /* removes all objects from list */
+    public void clear() {
+        user_objects.clear();
     }
     /* removes specified object from list */
     public boolean remove(String object_name) {
         try {
-            user_objects.remove(object_name);
+            user_objects.remove(object_name); // todo: remove auto-update?
             return true;
         }catch(NullPointerException e) { /* object_name = null */
             return false;
@@ -61,10 +96,34 @@ public class StoredObjects {
     }
     /* loads objects from file */
     public void loadObjects(String file_name) {
-        
+        try {
+            FileReader file = new FileReader(file_name);
+            BufferedReader read_objects = new BufferedReader(file);
+            String line = "";
+            while ((line = read_objects.readLine()) != null) {
+                try { /* read in format "object_name: StringRepresentation" */
+                    String object_name = line.substring(0, line.indexOf(":"));
+                    Object object = get(line.substring(line.indexOf(":") + 2));
+                    put(object_name, object);
+                } catch (StringIndexOutOfBoundsException e) { /* no value defined */
+                }
+            }
+        } catch (IOException e) {} // todo: error-catching?
     }
     /* writes objects to file */
     public void writeObjects(String file_name) {
-
+        try {
+            FileWriter file = new FileWriter(file_name);
+            BufferedWriter write_objects = new BufferedWriter(file);
+            Enumeration<String> objects = user_objects.keys();
+            while(objects.hasMoreElements()) {
+                String object_name = objects.nextElement();
+                /* write in format "object_name: StringRepresentation" */
+                write_objects.write(object_name + ": " +
+                        (new GeometryObject(user_objects.get(object_name))).toString());
+                write_objects.newLine();
+            }
+            write_objects.close();
+        } catch (IOException e) { } // todo: error catching?
     }
 }
