8004a7c134c24439c17bd49e6343abf3d3bd0199
stefan4472 <stefan.kussmaul@gmail.com>
2015-03-27 20:59:28 -0400

    Refactoring and Added Methods

--- src/plainsimple/geometrytoolkit/Point2D.java
+++ src/plainsimple/geometrytoolkit/Point2D.java
@@ -4,6 +4,10 @@ public class Point2D {
   private double x;
   private double y;
   private String name;
+    public Point2D() {
+        x = 0.0;
+        y = 0.0;
+    }
   public Point2D(double x, double y) {
     this.x = x;
     this.y = y;
@@ -13,21 +17,21 @@ public class Point2D {
     /* sets name of point */
     public void setName(String s) { name = s; }
   /* returns x */
-  public double getX() {
+  public double x() {
     return x;
   }
   /* returns y */
-  public double getY() {
+  public double y() {
     return y;
   }
   /* returns String representation of point */
-  public String getPointString() {
+  public String getCoordinates() {
     return "(" + x + ", " + y + ")";
   }
   /* returns distance between points */
-  public double getDistance(Point3D point_2) {
-    return Math.sqrt(Math.pow((x - point_2.getX()), 2) +
-                     Math.pow((y - point_2.getY()), 2));
+  public double getDistance(Point2D point_2) {
+    return Math.sqrt(Math.pow((x - point_2.x()), 2) +
+                     Math.pow((y - point_2.y()), 2));
   }
 
 }
